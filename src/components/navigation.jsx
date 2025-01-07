65f00a9c77cb4bf99e9230a37be2a729e6392f31
KitsuneSiho <x-intercept@naver.com>
2025-01-07 21:30:40 +0900

    [추가점] - 메인화면 그리드 UI - 이미지 소스
    [변경점]
    - 네비게이션 바 카테고리리

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -11,15 +11,14 @@ function Navigation() {
     <div className='navigation'>
       <Navbar bg="light" data-bs-theme="light" fixed='top'>{/*백그라운드 밝게, 상단 고정*/}
         <Container>
-          <Navbar.Brand href="home">ModHub</Navbar.Brand>
+          <Navbar.Brand href="home">Game Hub</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="game-mod">게임모드</Nav.Link>
-            <Nav.Link href="community">커뮤니티</Nav.Link>
+            <Nav.Link href="notice">공지사항</Nav.Link>
+            <Nav.Link href="community">자유게시판</Nav.Link>
             <Nav.Link href="test">테스트</Nav.Link>
-            <Nav.Link href="editor">글쓰기 에디터</Nav.Link>
-            <Nav.Link href="board">게시판</Nav.Link>
           </Nav>
           <Nav className="ms-auto">
+          <Nav.Link href="weather">날씨위젯 들어갈곳</Nav.Link>
             <Nav.Link href="login">로그인</Nav.Link>
           </Nav>
         </Container>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import Poster from "./poster"
+import palworldImage from '../assets/images/game-images/Palworld.jpg'
+import minecraftImage from '../assets/images/game-images/Minecraft.png'
+import pubgImage from '../assets/images/game-images/Pubg.png'
+import humanFallFlatImage from '../assets/images/game-images/HumanFF.jpg'
 
- const Home= () =>{
+const posters = [
+    { image: palworldImage, title:"팰월드", link:"/palworld"},
+    { image: minecraftImage, title:"마인크래프트", link:"/minecraft"},
+    { image: pubgImage, title:"배틀그라운드", link:"/pubg"},
+    { image: humanFallFlatImage, title:"Human Fall Flat", link:"/human-fall-flat" }
+]
+const Home= () =>{
     return(
-<>
-<h2>모드 게임허브</h2>
-</>
-    )
- }
+        <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap"}}>
+            {posters.map((poster, index) => (
+                <Poster
+                key={index}
+                image={poster.image}
+                title={poster.title}
+                link={poster.link}
+                />
+            ))}
+        </div>
+
+    );
+ };
 
 export default Home;
