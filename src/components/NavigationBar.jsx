e991132bd0a49a796ff5061f444df864401f089c
KitsuneSiho <x-intercept@naver.com>
2025-03-13 20:50:10 +0900

    03/13 - 네비게이션바 수정 - 레이아웃 제거

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,32 +1,33 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Button } from 'bootstrap';
-import WeatherBox from './WeatherBox';
+import './NavigationBar.css';
+import gamehub from '../assets/images/logo/gamehub-logo.jpg'; // 로고 이미지 임포트
 
-function Navigation() {
+const NavigationBar = () => {
   return (
-    <div className='navigation'>
-      <Navbar bg="light" data-bs-theme="light" fixed='top'>{/*백그라운드 밝게, 상단 고정*/}
-        <Container>
-          <Navbar.Brand href="home">Game Hub</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="notice">공지사항</Nav.Link>
-            <Nav.Link href="community">자유게시판</Nav.Link>
-            <Nav.Link href="test">테스트</Nav.Link>
-            <Nav.Link href="board">게시판</Nav.Link>
+    <Navbar expand="lg" className="bg-body-tertiary" id="nav-bar-bg">
+      <Container fluid className="nav-bar-container">
+        {/* 브랜드 로고 */}
+        <Navbar.Brand as={NavLink} to="/">
+          <img src={gamehub} alt="Game Hub" width="auto" height="50" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto"> {/* 왼쪽 정렬 */}
+            <Nav.Link as={NavLink} to="/notice" className="custom-nav-link">공지사항</Nav.Link>
+            <Nav.Link as={NavLink} to="/community" className="custom-nav-link">자유게시판</Nav.Link>
+            <Nav.Link as={NavLink} to="/test" className="custom-nav-link">테스트</Nav.Link>
+            <Nav.Link as={NavLink} to="/board" className="custom-nav-link">게시판</Nav.Link>
           </Nav>
-          <Nav className="ms-auto">
-          <WeatherBox />
-            <Nav.Link href="login">로그인</Nav.Link>
+          <Nav className="ms-auto"> {/* 오른쪽 정렬 */}
+            <Nav.Link as={NavLink} to="/login" className="custom-nav-link">로그인</Nav.Link>
           </Nav>
-        </Container>
-      </Navbar>
-    </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-}
+};
 
-export default Navigation;
+export default NavigationBar;
